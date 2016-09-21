cc44923ac586d03c1cddb679ba0c5f4d612cc839
Tom Oram <tom@x2k.co.uk>
2016-09-21 22:35:20 +0100

    Add AbstractAggregateTest
    All aggregates require the EventBus to be configured.

--- test/unit/Domain/ProjectTest.php
+++ test/unit/Domain/ProjectTest.php
@@ -2,30 +2,21 @@
 
 namespace test\unit\TomPHP\TimeTracker\Domain;
 
-use TomPHP\TimeTracker\Domain\EventBus;
-use TomPHP\TimeTracker\Domain\EventHandler;
 use TomPHP\TimeTracker\Domain\Events\ProjectCreated;
 use TomPHP\TimeTracker\Domain\Project;
 use TomPHP\TimeTracker\Domain\ProjectId;
 
-final class ProjectTest extends \PHPUnit_Framework_TestCase
+final class ProjectTest extends AbstractAggregateTest
 {
-    protected function tearDown()
-    {
-        EventBus::clearHandlers();
-    }
-
     /** @test */
     public function on_create_it_publishes_a_project_created_event()
     {
-        $handler = $this->prophesize(EventHandler::class);
-        EventBus::addHandler($handler->reveal());
-
         $projectId = ProjectId::generate();
 
         Project::create($projectId, 'Example Project');
 
-        $handler->handle(new ProjectCreated($projectId, 'Example Project'))
-                ->shouldHaveBeenCalled();
+        $this->handler()
+            ->handle(new ProjectCreated($projectId, 'Example Project'))
+            ->shouldHaveBeenCalled();
     }
 }
--- test/unit/Domain/TimeEntryTest.php
+++ test/unit/Domain/TimeEntryTest.php
@@ -3,27 +3,17 @@
 namespace test\unit\TomPHP\TimeTracker\Domain;
 
 use TomPHP\TimeTracker\Domain\Date;
-use TomPHP\TimeTracker\Domain\EventBus;
-use TomPHP\TimeTracker\Domain\EventHandler;
 use TomPHP\TimeTracker\Domain\Events\TimeEntryLogged;
 use TomPHP\TimeTracker\Domain\Period;
 use TomPHP\TimeTracker\Domain\ProjectId;
 use TomPHP\TimeTracker\Domain\TimeEntry;
 use TomPHP\TimeTracker\Domain\UserId;
 
-final class TimeEntryTest extends \PHPUnit_Framework_TestCase
+final class TimeEntryTest extends AbstractAggregateTest
 {
-    protected function tearDown()
-    {
-        EventBus::clearHandlers();
-    }
-
     /** @test */
     public function on_log_it_publishes_a_time_entry_logged_event()
     {
-        $handler = $this->prophesize(EventHandler::class);
-        EventBus::addHandler($handler->reveal());
-
         $userId      = UserId::generate();
         $projectId   = ProjectId::generate();
         $date        = Date::fromString('2016-09-20');
@@ -32,12 +22,13 @@ final class TimeEntryTest extends \PHPUnit_Framework_TestCase
 
         TimeEntry::log($userId, $projectId, $date, $period, $description);
 
-        $handler->handle(new TimeEntryLogged(
-            $userId,
-            $projectId,
-            $date,
-            $period,
-            $description
-        ))->shouldHaveBeenCalled();
+        $this->handler()
+            ->handle(new TimeEntryLogged(
+                $userId,
+                $projectId,
+                $date,
+                $period,
+                $description
+            ))->shouldHaveBeenCalled();
     }
 }
