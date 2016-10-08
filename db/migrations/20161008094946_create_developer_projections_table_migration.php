91b5d158fe61116450e73d420bd96e3662b52b95
Tom Oram <tom@codept.com>
2016-10-08 22:52:39 +0100

    Fix migration name

--- db/migrations/20161008094946_create_developer_projections_table_migration.php
+++ db/migrations/20161008094946_create_developer_projections_table_migration.php
@@ -2,7 +2,7 @@
 
 use Phinx\Migration\AbstractMigration;
 
-class CreateDeveloperProjectsTableMigration extends AbstractMigration
+class CreateDeveloperProjectionsTableMigration extends AbstractMigration
 {
     public function change()
     {
