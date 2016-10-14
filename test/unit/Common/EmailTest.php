e47c82040a146171248fb12166e0921f623ae671
Tom Oram <tom@codept.com>
2016-10-15 00:03:19 +0100

    Add Common\Email
    Simple value object, contains no validation yet.
    
    [#132199459]

---
+++ test/unit/Common/EmailTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace test\unit\TomPHP\TimeTracker\Common;
+
+use TomPHP\TimeTracker\Common\Email;
+
+final class EmailTest extends \PHPUnit_Framework_TestCase
+{
+    /** @test */
+    public function it_can_be_converted_from_and_to_a_string()
+    {
+        assertSame('tom@example.com', (string) Email::fromString('tom@example.com'));
+    }
+}
