dad0a5f3a399292fc1afce68c6de21b544c19e4e
Tom Oram <tom@codept.com>
2016-10-21 12:08:29 +0100

    Make ci/unit.sh change dir to the project root
    [#132200995]

--- ci/unit.sh
+++ ci/unit.sh
@@ -1,5 +1,10 @@
 #!/bin/sh -xe
 
+SCRIPT_DIR=`dirname $0`
+PROJECT_DIR=`dirname $SCRIPT_DIR`
+
+pushd $PROJECT_DIR
+
 pwd
 
 composer install --no-plugins --no-scripts
@@ -9,3 +14,5 @@ php-cs-fixer fix --dry-run
 vendor/bin/phpunit --test-suite unit
 vendor/bin/behat -p features
 vendor/bin/behat -p integration
+
+popd
