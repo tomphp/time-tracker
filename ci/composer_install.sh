b99e101cbf0ea6528b1e97c91c4814c9022b3972
Tom Oram <tom@codept.com>
2016-10-29 09:55:42 +0100

    Add S3 caching of the composer vendor dir
    [#133381413]

--- ci/composer_install.sh
+++ ci/composer_install.sh
@@ -1,5 +1,27 @@
 # bash -ex
 
+cacheFile=time-tracker-vendor.tar.gz
+bucket=ci-composer-cache
+resource="/${bucket}/${cacheFile}"
+contentType="application/x-compressed-tar"
+dateValue=`date -R`
+stringToSign="PUT\n\n${contentType}\n${dateValue}\n${resource}"
+s3Key=$S3_KEY
+s3Secret=$S3_SECRET
+signature=`echo -en ${stringToSign} | openssl sha1 -hmac ${s3Secret} -binary | base64`
+
+stringToSign="GET\n\ntext/plain\n${dateValue}\n${resource}"
+
+curl -H "Date: ${dateValue}" \
+     -H "Content-Type: ${contentType}" \
+     -H "Authorization: AWS ${s3Key}:${signature}" \
+     "https://s3-us-west-2.amazonaws.com/${resource}" \
+     -o "$cacheFile"
+
+if [ -e "$cacheFile" ]; then
+  tar xvzf "$cacheFile"
+fi
+
 git clone "$SOURCE" "$DESTINATION"
 cd "$DESTINATION"
 
@@ -8,3 +30,13 @@ if [ "$MODE" = "production" ]; then
 else
   composer install --no-interaction --no-ansi
 fi
+
+# Backup to S3
+tar cvzf "$cacheFile" vendor/
+
+curl -X PUT -T "${cacheFile}" \
+  -H "Host: ${bucket}.s3.amazonaws.com" \
+  -H "Date: ${dateValue}" \
+  -H "Content-Type: ${contentType}" \
+  -H "Authorization: AWS ${s3Key}:${signature}" \
+  https://${bucket}.s3.amazonaws.com/${cacheFile}
