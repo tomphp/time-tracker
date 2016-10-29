881970ad8363650f741b4b0043844174a1e07147
Tom Oram <tom@codept.com>
2016-10-29 12:07:16 +0100

    Read composer cache input
    [#133381413]

--- ci/composer_install.sh
+++ ci/composer_install.sh
@@ -6,7 +6,7 @@ git clone "$SOURCE" "$DESTINATION"
 
 cd "$DESTINATION"
 
-! tar xzf vendor-cache.tgz
+! tar xzf "$CACHE_DIR/vendor-cache.tgz"
 
 if [ "$MODE" = "production" ]; then
   composer install --no-interaction --no-progress --no-suggest --no-dev --no-scripts --no-plugins --optimize-autoloader
